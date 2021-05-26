fdc2ef20b92626efae1c5ccb4f95a69c6b8627a9
gabrieldausque <gabrieldausque.pro@gmail.com>
2021-05-26 14:24:53 +0200

    #225 create drop zone component for simplification

---
+++ packages/frontend/src/components/contentComponents/Documents/BlockDropZone.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    export let component;
+    export let isBefore;
+    export let zone;
+
+
+    //TODO : raise helper ....
+</script>
+
+<div class="drop-zone"
+     data-zone="{zone}"
+     data-order="{typeof component.order === 'number'? (isBefore?component.order - 1:component.order + 1):0}"
+     on:dragenter={(event) => dispatch('dragenter', event)}
+     on:dragleave={(event) => dispatch('dragleave', event)}
+     on:drop={(event) => dispatch('drop', event)}
+     on:dragover={(event) => { event.preventDefault(); return false;}}
+></div>
