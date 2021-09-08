910c62d4c3d7f24ede76fb10c4dd30c0f620193b
gabriel.dausque <gabrieldausque.pro@gmail.com>
2021-09-08 09:04:06 +0200

    Comit after joomla install

---
+++ PHP/joomla-teama/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-09-08 05:26:12 UTC
+#Software: Joomla! 4.0.2 Stable [ Furaha ] 24-August-2021 19:54 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-09-08T05:26:12+00:00	INFO 127.0.0.1	joomlafailure	Le nom d'utilisateur ne correspond pas au mot de passe, ou vous n'avez pas encore de compte.
