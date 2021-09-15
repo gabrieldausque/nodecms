8dd3cf5ad89352942cfc0ee796997df78b4aed1a
gabriel.dausque <gabrieldausque.pro@gmail.com>
2021-09-15 23:08:29 +0200

    update to 4.0.3 + frontend display for component

---
+++ PHP/joomla-teama/administrator/logs/joomla_update.php
@@ -0,0 +1,24 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-09-15 12:49:01 UTC
+#Software: Joomla! 4.0.2 Stable [ Furaha ] 24-August-2021 19:54 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-09-15T12:49:01+00:00	INFO 127.0.0.1	update	Mise à jour lancée par l'utilisateur admin_teama (461). L'ancienne version est &#x200E;4.0.2.
+2021-09-15T12:49:04+00:00	INFO 127.0.0.1	update	Téléchargement du fichier de mise à jour depuis https://downloads.joomla.org/cms/joomla4/4-0-3/Joomla_4.0.3-Stable-Update_Package.zip.
+2021-09-15T12:49:26+00:00	INFO 127.0.0.1	update	Fichier Joomla_4.0.3-Stable-Update_Package.zip téléchargé.
+2021-09-15T12:49:26+00:00	INFO 127.0.0.1	update	Lancement de l'installation de la nouvelle version.
+2021-09-15T12:55:54+00:00	INFO 127.0.0.1	update	Conclusion de l'installation.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-04. Texte de la requête: DELETE FROM `#__update_sites_extensions`  WHERE `update_site_id` IN (SELECT `upd.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-04. Texte de la requête: DELETE FROM `#__update_sites`  WHERE `location` = 'https://raw.githubusercontent.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-04. Texte de la requête: DELETE FROM `#__extensions`  WHERE `type` = 'package' AND `element` = 'pkg_searc.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'PUBLISH' WHERE `title`= 'Publish.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'UNPUBLISH' WHERE `title`= 'Unpub.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'TRASH' WHERE `title`= 'Trash';.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'ARCHIVE' WHERE `title`= 'Archive.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'FEATURE' WHERE `title`= 'Feature.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'UNFEATURE' WHERE `title`= 'Unfea.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Requête lancée à partir du fichier 4.0.3-2021-09-05. Texte de la requête: UPDATE `#__workflow_transitions` SET `title` = 'PUBLISH_AND_FEATURE' WHERE `titl.
+2021-09-15T12:55:55+00:00	INFO 127.0.0.1	update	Effacement des fichiers et dossiers à supprimer.
+2021-09-15T12:56:05+00:00	INFO 127.0.0.1	update	Mise en ordre après installation.
+2021-09-15T12:56:05+00:00	INFO 127.0.0.1	update	La mise à jour vers la version 4.0.3 est achevée.
